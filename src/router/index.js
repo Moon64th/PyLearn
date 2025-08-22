9ab9c71272f91fed41d24f05ba518c2b59208d23
leandro.mani <leandro.mani@liip.ch>
2025-08-22 08:17:13 +0200

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from "../views/home.vue";
+import About from "../views/about.vue";
+
+const routes = [{
+    path: "/",
+    name: "home",
+    component: Home
+},{
+    path: "/about",
+    name: "about",
+    component: About
+}
+]
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.VITE_BASE_URL),
+    routes
+})
+
+export default router
